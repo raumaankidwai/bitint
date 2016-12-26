cdcd819ab565ab179296e23962d3244eff6f9df2
Raumaan Kidwai <raumaankidwai@gmail.com>
2016-12-25 19:04:26 -0500

    Update bitint.js

--- bitint.js
+++ bitint.js
@@ -1,11 +1,46 @@
-class BitInt {
-	constructor (bits) {
-		this.bits = bits;
-	}
+const bi = {
+	clean: (n) => {
+		while (!n[0]) {
+			n = n.slice(1);
+		}
+		
+		return n;
+	},
 	
-	static add (n1, n2) {
-		var b1 = n1.bits.reverse();
-		var b2 = n2.bits.reverse();
+	eq: (n1, n2) => {
+		for (var i = 0; i < Math.max(n1.length, n2.length); i ++) {
+			if (n1[i] != n2[i]) {
+				return false;
+			}
+		}
+		
+		return true;
+	},
+	
+	gt: (n1, n2) => {
+		var b1 = clean(n1);
+		var b2 = clean(n2);
+		
+		if (!b1.length) {
+			return false;
+		}
+		
+		if (!b2.length) {
+			return true;
+		}
+		
+		if (b1.length == b2.length) {
+			return bi.gt(b1.slice(1), b2.slice(1));
+		}
+		
+		return b1.length > b2.length;
+		
+		return b1.length ? b2.length ? b1.length == b2.length ? bi.gt(b1.slice(1), b2.slice(1)) : b1.length > b2.length : true : false;
+	},
+	
+	add: (n1, n2) => {
+		var b1 = n1.reverse();
+		var b2 = n2.reverse();
 		
 		var b = [];
 		var carry = 0;
@@ -18,6 +53,27 @@ class BitInt {
 			carry = +(n > 1);
 		}
 		
-		return b.reverse();
+		return bi.clean(b.reverse());
+	},
+	
+	sub: (n1, n2) => {
+		var b1 = n1.reverse();
+		var b2 = n2.reverse();
+		
+		var b = [];
+		var take = 0;
+		
+		for (var i = 0; i < b1.length; i ++) {
+			var n = ~~b1[i] - take - ~~b2[i];
+			
+			if (n < 0) {
+				n += 2;
+				take = 1;
+			}
+			
+			b.push(n);
+		}
+		
+		return bi.clean(b.reverse());
 	}
-}
+};
