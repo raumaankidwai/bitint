dc7593614432907d4efbc5018ff846835dad3038
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-03-28 11:08:17 -0400

    Update bitint.js

--- bitint.js
+++ bitint.js
@@ -200,14 +200,14 @@ const bi = {
 		var h2 = n2.slice(0, -m);
 		var l2 = n2.slice(-m);
 		
-		var z0 = bi.karatsuba8192(l1, l2);
-		var z1 = bi.karatsuba8192(bi.to8192(bi.add(bi.from8192(l1), bi.from8192(h1))), bi.to8192(bi.add(bi.from8192(l2), bi.from8192(h2))));
-		var z2 = bi.karatsuba8192(h1, h2);
+		var z0 = bi.karatsuba8192base(l1, l2);
+		var z1 = bi.karatsuba8192base(bi.to8192(bi.add(bi.from8192(l1), bi.from8192(h1))), bi.to8192(bi.add(bi.from8192(l2), bi.from8192(h2))));
+		var z2 = bi.karatsuba8192base(h1, h2);
 		
 		return bi.to8192(bi.add(bi.add(bi.from8192(z2).concat(Array(2 * m).fill(0)), bi.sub(bi.sub(bi.from8192(z1), bi.from8192(z2)), bi.from8192(z0)).concat(Array(m).fill(0))), bi.from8192(z0)));
 	},
 	
-	karatsuba8192: (a, b) => bi.from8192(bi.karatsuba8192base(a, b)),
+	karatsuba8192: (a, b) => bi.from8192(bi.karatsuba8192base(bi.to8192(a), bi.to8192(b))),
 	
 	divbase: (n1, n2) => {
 		var n = [n1[0]];
